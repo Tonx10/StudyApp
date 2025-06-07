cf708461d3d435bf0fa161ea03f9733234b811c4
Tonx10 <nawatwancham@gmail.com>
2025-06-08 03:19:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/studyapp/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.studyapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.studyapp.ui.theme.StudyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StudyAppTheme {
+//                DashboardScreen()
+//                 CreateTaskScreen()
+                 PomodoroScreen()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun DashboardScreen() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text("Welcome Lisa!", fontSize = 24.sp)
+        Text("XP: 150 / 200")
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text("Today's Tasks:")
+        Text("- Finish Assignment")
+        Text("- Review Lecture Notes")
+        Text("- Group Project Meeting")
+    }
+}
+
+
+@Composable
+fun CreateTaskScreen() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text("Add New Task", fontSize = 24.sp)
+        Spacer(modifier = Modifier.height(8.dp))
+        Text("Task Name: _______")
+        Spacer(modifier = Modifier.height(8.dp))
+        Text("Due Date: ________")
+        Spacer(modifier = Modifier.height(16.dp))
+        Text("👉 [Add Task Button]")
+    }
+}
+
+@Composable
+fun PomodoroScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Pomodoro Timer", fontSize = 24.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text("25:00", fontSize = 48.sp)
+        Spacer(modifier = Modifier.height(24.dp))
+        Text("👉 [Start Button]")
+    }
+}
